5a20bd181c361644c38e157d4b43d847c73ada33
Brayden Tomlinson <humortronic@gmail.com>
2024-12-03 12:41:15 +1030

    Added changes to code

---
+++ UI.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+import serial
+
+def find_arduino_port():
+    ports = list(serial.tools.list_ports.comports())
+    for port in ports:
+        if 'Arduino' in port.description or 'usbmodem' in port.device:
+            return port.device
+    return None
+
+def initialize_serial_connection():
+    global ser
+    port  = find_arduino_port()
+    if port:
+        try:
+            ser = serial.Serial(port, 9600)
+        except Exception as e:
+            ser = None
+            print(f"Failed to connect to Arduino: {e}")
+    else:
+        ser = None
+        print("Arduino not found.")
+
+initialize_serial_connection()
+
